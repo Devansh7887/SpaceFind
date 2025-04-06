f43f1100dac969779ff17a1ac253936d9d566f4f
Devansh Agrawal <165183181+Devansh7887@users.noreply.github.com>
2025-04-06 09:06:52 +0530

    upload data on cloud

--- init/index.js
+++ init/index.js
@@ -1,27 +1,69 @@
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 
+const dbUrl = "mongodb+srv://devanshagrawal:8Ef7gRg0KxqTnhNO@cluster0.qhwqrij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mapToken = "pk.eyJ1IjoiaW5zcGlyZTI1NiIsImEiOiJjbThwZzkzb2cwMXhwMmlzY3VrNjQ3dGk4In0.XK929l_AotYhiPcPU2diWw";
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("connected to DB"))
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({...obj, owner: "67df8ecdfc2dcd4eb408fa24"}))
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+  
+  for (let obj of initData.data) {
+    const geoData = await geocodingClient
+      .forwardGeocode({
+        query: obj.location,
+        limit: 1,
+      })
+      .send();
+
+    const listing = new Listing({
+      ...obj,
+      owner: "67df8ecdfc2dcd4eb408fa24",
+      geometry: geoData.body.features[0].geometry,
+    });
+
+    await listing.save();
+  }
+
+  console.log("All listings added with dynamic coordinates");
 };
 
 initDB();
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
+
+// main()
+//   .then(() => {
+//     console.log("connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async () => {
+//   await Listing.deleteMany({});
+//   initData.data = initData.data.map((obj) => ({...obj, owner: "67df8ecdfc2dcd4eb408fa24"}))
+//   await Listing.insertMany(initData.data);
+//   console.log("data was initialized");
+// };
+
+// initDB();
